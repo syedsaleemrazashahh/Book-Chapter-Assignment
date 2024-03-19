06d10e6a940490a29dcd98ba06a52a4f158a8f8f
Syed Saleem Raza Shahit <syedsaleemrazashah69@gmail.com>
2024-03-19 07:36:05 +0500

    Chapter 5 done

--- app.js
+++ app.js
@@ -86,3 +86,26 @@ document.getElementById("E").innerHTML =
   "Variable names should not be JS keywords";
 
 // Chapter no 5
+
+let str1 = "Saleem"
+let str2 = " Raza"
+let str3 = str1.concat(str2)
+console.log(str3);
+
+
+
+
+
+
+  // Cost of one movie ticket is 600 PKR
+  let ticketPrice = 600;
+
+  // Number of tickets to buy
+  let numberOfTickets = 5;
+
+  // Calculate the total cost
+  let totalCost = ticketPrice * numberOfTickets;
+
+  // Output the result
+  console.log(`Cost of one movie ticket is  ${ ticketPrice } `);
+  console.log(`Cost of buying  ${numberOfTickets} tickets to a movie is ${ totalCost} `);
